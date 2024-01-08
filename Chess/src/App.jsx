23788a22c8463bb3a6dcb923b6f9d3d72d9094ac
Elite Carlson <elitecarlson@gmail.com>
2024-01-08 03:54:16 +0200

    feat: init

--- Chess/src/App.jsx
+++ Chess/src/App.jsx
@@ -1,7 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+import Index from "./Pages/Index";
+
 export default function App(){
   return(
-    <>
-    <p>Hello world</p>
-    </>
+    <Routes>
+      <Route path="/" element={<Index/>}></Route>
+    </Routes>
   );
 }
