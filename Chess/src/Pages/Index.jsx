e25937627c930e1f4fa602ec6ee077995b5dea2c
Elite Carlson <elitecarlson@gmail.com>
2024-01-08 16:56:15 +0200

    feat: board setup

--- Chess/src/Pages/Index.jsx
+++ Chess/src/Pages/Index.jsx
@@ -1,5 +1,49 @@
+import "../assets/css/index.css"
+import "../assets/css/Board.css"
 export default function Index(){
+    function RenderPieces(){
+        const startingPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+        const files = ["a","b","c","d","e","f","g","h"];
+        const pieceFEN = {
+            "r" : "br",
+            "n" : "bn",
+            "b" : "bb",
+            "q" : "bq",
+            "k" : "bk",
+            "p" : "bp",
+            "R" : "br",
+            "N" : "wn",
+            "B" : "wb",
+            "Q" : "wq",
+            "K" : "wk",
+            "P" : "wp",
+        }
+        const pieces = startingPosition.split(" ")[0];
+        let file = 0, rank = 8;
+        pieces.split("").forEach(char => {
+            if (char == "/") {
+                file = 0;
+                rank--;
+            }else{
+                if(!isNaN(char)){
+                    file += parseInt(char);
+                }else{
+                    const piecePosition = files[file]+rank;
+                    // const piece = JSON.parse(pieceFEN.char);
+                    // console.log(piece)
+                    file++
+                }
+            }
+        });
+    }
+    RenderPieces();
     return(
-        <p>Hello world</p>
+        <>
+        <div className="body">
+            <div className="board">
+                <div className="square"></div>
+            </div>
+        </div>
+        </>
     );
 }
