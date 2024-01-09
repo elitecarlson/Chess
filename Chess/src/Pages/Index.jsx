68bd68350955a2cabe075d4d3fc96ba6eb8d611a
Elite Carlson <elitecarlson@gmail.com>
2024-01-09 14:52:46 +0200

    feat: loading fen and flipping and unflipping board

--- Chess/src/Pages/Index.jsx
+++ Chess/src/Pages/Index.jsx
@@ -1,8 +1,14 @@
 import "../assets/css/index.css"
 import "../assets/css/Board.css"
+import { useEffect, useRef, useState } from "react";
+
 export default function Index(){
+    const body = useRef();
+    const board = useRef();
+    const [flipped, setFlipped] = useState(false);
+    const [startingPosition, setStartingPosition] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    
     function RenderPieces(){
-        const startingPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
         const files = ["a","b","c","d","e","f","g","h"];
         const pieceFEN = {
             "r" : "br",
@@ -11,13 +17,14 @@ export default function Index(){
             "q" : "bq",
             "k" : "bk",
             "p" : "bp",
-            "R" : "br",
+            "R" : "wr",
             "N" : "wn",
             "B" : "wb",
             "Q" : "wq",
             "K" : "wk",
             "P" : "wp",
         }
+        board.current.innerHTML = '';
         const pieces = startingPosition.split(" ")[0];
         let file = 0, rank = 8;
         pieces.split("").forEach(char => {
@@ -29,19 +36,31 @@ export default function Index(){
                     file += parseInt(char);
                 }else{
                     const piecePosition = files[file]+rank;
-                    // const piece = JSON.parse(pieceFEN.char);
-                    // console.log(piece)
+                    const piece = pieceFEN[char];
+                    
+                    let piececlassName
+                    if (flipped) {
+                        piececlassName = `square flipped-${piecePosition} ${piece}`;
+                    }else{
+                        piececlassName = `square ${piecePosition} ${piece}`;
+                    }
+
+                    const pieceDiv = document.createElement("div");
+                    pieceDiv.className = piececlassName;
+                    board.current.appendChild(pieceDiv)
                     file++
                 }
             }
         });
     }
-    RenderPieces();
+    
+    useEffect(()=>{
+        RenderPieces();
+    },[startingPosition]);
     return(
         <>
-        <div className="body">
-            <div className="board">
-                <div className="square"></div>
+        <div className="body" ref={body}>
+            <div className="board" ref={board}>
             </div>
         </div>
         </>
